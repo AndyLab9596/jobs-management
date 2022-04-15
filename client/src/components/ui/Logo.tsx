2bcb5c2a1914ab1ddb1e4bd6e3468e7c9874c3c0
andy <vy.chung@hdwebsoft.co>
2022-04-15 10:31:49 +0700

    basis landing page

---
+++ client/src/components/ui/Logo.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from '../../assets/images/logo.svg';
+
+const Logo = () => {
+    return (
+        <img src={logo} alt='jobify' className='block object-cover border-none' />
+    )
+}
+
+export default Logo
---
+++ client/src/pages/landing/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Logo from '../../components/ui/Logo';
+import main from '../../assets/images/main.svg'
+
+const LandingPage = () => {
+    return (
+        <main>
+            <nav className='max-w-7xl w-[90vw] flex items-center mx-auto h-24'>
+                <Logo />
+            </nav>
+
+            {/* Info */}
+            <div className='custom-container grid grid-cols-1 lg:grid-cols-2 lg:gap-48 items-center -mt-12 min-height'>
+
+                <div className='flex flex-col'>
+                    {/* Title */}
+                    <h1 className='font-bold'>
+                        Job <span className='text-sky-500'>Tracking</span> App
+                    </h1>
+                    {/* Description */}
+                    <p className='text-gray-700'>
+                        I'm baby wayfarers hoodie next level taiyaki brooklyn cliche blue bottle single-origin coffee chia.
+                        Aesthetic post-ironic venmo, quinoa lo-fi tote bag adaptogen everyday carry meggings +1 brunch narwhal.
+                    </p>
+                    <button className='cursor-pointer text-white bg-sky-600 border-transparent rounded-md px-[20px] py-[8px] text-lg inline-block max-w-max shadow-lg hover:shadow-2xl hover:bg-sky-800 transition'>
+                        Login/Register
+                    </button>
+                    
+                </div>
+
+                <img src={main} alt="landing" className='hidden lg:block lg:object-cover lg:w-full ' />
+
+            </div>
+        </main>
+    )
+}
+
+export default LandingPage
